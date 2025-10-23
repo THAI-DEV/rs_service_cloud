e0de2b8bedd339c3787be179bddbbeee8edf5839
Ruangdech Kurunat <ruangdech@gmail.com>
2025-10-23 19:19:26 +0700

    refactor: enhance logging configuration and update log messages in app_serve.rs

--- src/app/app_serve.rs
+++ src/app/app_serve.rs
@@ -1,7 +1,11 @@
 use axum::{Router, response::Html, routing::get};
+use log::info;
 
 #[tokio::main]
 pub async fn run() {
+    // Initialize tracing for logging
+    log4rs::init_file("config_log.yaml", Default::default()).unwrap();
+
     // build our application with a route
     let routes = Router::new().route("/", get(handler));
 
@@ -13,17 +17,19 @@ pub async fn run() {
     // axum::serve(listener, routes).await.unwrap()
 
     let shutdown = async {
-        // info!("Press Ctrl+C to stop the server");
+        info!("Press Ctrl+C to stop the server");
 
         tokio::signal::ctrl_c()
             .await
             .expect("failed to install Ctrl+C handler");
-        // info!(".. Shutting down server ...");
+        info!(".. Shutting down server ...");
     };
 
     let server = axum::serve(listener, routes).with_graceful_shutdown(shutdown);
 
     server.await.unwrap();
+
+    info!("Server has been shut down gracefully.");
 }
 
 async fn handler() -> Html<&'static str> {
