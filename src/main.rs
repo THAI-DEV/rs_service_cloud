fbe1cc1bceb4ffebfa45f9aa65f0c52f01f95ef7
Ruangdech Kurunat <ruangdech@gmail.com>
2025-10-23 17:33:25 +0700

    refactor: move application logic to app_serve module and simplify main function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,4 @@
-use axum::{Router, response::Html, routing::get};
-
-#[tokio::main]
-async fn main() {
-    // build our application with a route
-    let app = Router::new().route("/", get(handler));
-
-    // run it
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:4000")
-        .await
-        .unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
-}
-
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+use rs_service_cloud::app::app_serve;
+fn main() {
+    app_serve::run();
 }
