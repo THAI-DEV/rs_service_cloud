8f2f715a5f00f132c269fc1b118e4f79bb8a0b06
Ruangdech Kurunat <ruangdech@gmail.com>
2025-10-23 17:11:09 +0700

    refactor: comment out unused code and implement basic routing with Axum

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-use std::time::Instant;
-
-use dechdev_rs::utils::date_time::calculate_elapsed_duration;
-use rs_service_cloud::app::app_server;
+use axum::{Router, response::Html, routing::get};
 
 #[tokio::main]
 async fn main() {
-    let start_time = Instant::now();
+    // let start_time = Instant::now();
 
     // let app_name = env!("CARGO_PKG_NAME"); // Get
     // let version = env!("CARGO_PKG_VERSION"); // Get the version from Cargo.toml
@@ -20,17 +17,43 @@ async fn main() {
     // println!(
     // "--- (Begin {app_name} v{version} | Built with {rustc_version} on {platform} at {build_date_time}) ---",
     // );
-    println!("--- Begin---",);
+    // println!("--- Begin---",);
 
-    println!();
+    // println!();
 
-    app_server::run().await;
+    // app_server::run().await;
 
-    println!();
+    // println!();
 
-    let (days, hours, minutes, seconds, millis) = calculate_elapsed_duration(start_time);
+    // let (days, hours, minutes, seconds, millis) = calculate_elapsed_duration(start_time);
 
-    println!(
-        "--- End ({days} days, {hours} hours, {minutes} minutes, {seconds} seconds, {millis} millis) ---"
-    );
+    // println!(
+    //     "--- End ({days} days, {hours} hours, {minutes} minutes, {seconds} seconds, {millis} millis) ---"
+    // );
+
+    let routes = Router::new().route("/", get(handler));
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    // axum::serve(listener, app).await.unwrap();
+
+    let shutdown = async {
+        // info!("Press Ctrl+C to stop the server");
+
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+        // info!(".. Shutting down server ...");
+    };
+
+    let server = axum::serve(listener, routes).with_graceful_shutdown(shutdown);
+
+    server.await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World! From DECH</h1>")
 }
