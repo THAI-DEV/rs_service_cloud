761b627cefb50502ee376e8a5fc5da656751348b
Ruangdech Kurunat <ruangdech@gmail.com>
2025-10-23 17:50:39 +0700

    refactor: hardcode RUSTC_VERSION in build.rs and comment out get_rustc_version function

--- build.rs
+++ build.rs
@@ -12,24 +12,24 @@ fn main() {
             .format("%H:%M:%S")
     );
 
-    println!("cargo:rustc-env=RUSTC_VERSION={}", get_rustc_version());
+    println!("cargo:rustc-env=RUSTC_VERSION={}", "1.90.0");
 }
 
-fn get_rustc_version() -> String {
-    let output = std::process::Command::new("rustc")
-        .arg("--version")
-        .output()
-        .expect("Failed to execute rustc command");
+// fn get_rustc_version() -> String {
+//     let output = std::process::Command::new("rustc")
+//         .arg("--version")
+//         .output()
+//         .expect("Failed to execute rustc command");
 
-    if output.status.success() {
-        let version = String::from_utf8_lossy(&output.stdout);
+//     if output.status.success() {
+//         let version = String::from_utf8_lossy(&output.stdout);
 
-        version
-            .split_whitespace()
-            .take(2)
-            .collect::<Vec<_>>()
-            .join(" ")
-    } else {
-        "Unknown".to_string()
-    }
-}
+//         version
+//             .split_whitespace()
+//             .take(2)
+//             .collect::<Vec<_>>()
+//             .join(" ")
+//     } else {
+//         "Unknown".to_string()
+//     }
+// }
