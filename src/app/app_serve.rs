df46a4e62b87dc7bafccf431ebf006e6ffcac175
Ruangdech Kurunat <ruangdech@gmail.com>
2026-03-03 00:14:43 +0700

    feat: add Redis integration and configuration management with dotenv support

--- src/app/app_serve.rs
+++ src/app/app_serve.rs
@@ -1,9 +1,12 @@
+extern crate redis;
+use redis::Commands;
+
 use std::sync::{Mutex, OnceLock};
 
 use axum::{Router, response::Html, routing::get};
 use log::info;
 
-use crate::business::model::AppInfoModel;
+use crate::{app::config::config_env, business::model::AppInfoModel};
 
 static APP_INFO_DATA: OnceLock<Mutex<AppInfoModel>> = OnceLock::new();
 
@@ -59,4 +62,19 @@ fn init_app_info_data() -> &'static Mutex<AppInfoModel> {
                 .with_timezone(&chrono::FixedOffset::east_opt(7 * 3600).expect("Invalid offset")),
         })
     })
+}
+
+pub fn run_poc() {
+    let i = fetch_an_integer().unwrap();
+    println!("{}", i);
+}
+
+fn fetch_an_integer() -> redis::RedisResult<isize> {
+    let client = redis::Client::open(config_env::get_config_env().redis_url.clone())?;
+    let mut con = client.get_connection()?;
+
+    // throw away the result, just make sure it does not fail
+    let _: () = con.set("answer", 42)?;
+
+    con.get("answer")
 }
---
+++ src/app/config/config_env.rs
@@ -0,0 +1,27 @@
+use serde::Serialize;
+use std::sync::OnceLock;
+
+static LOAD_ONCE: OnceLock<ConfigEnv> = OnceLock::new();
+
+pub fn get_config_env() -> &'static ConfigEnv {
+    LOAD_ONCE.get_or_init(|| {
+        dotenvy::dotenv().expect("Failed to load .env file");
+
+        let redis_url = dotenvy::var("REDIS_URL").expect("REDIS_URL environment variable not set");
+
+        let result = ConfigEnv {
+            redis_url: redis_url,
+        };
+        let result_json = serde_json::to_string_pretty(&result).unwrap();
+
+        println!("**** Loading Config Env **** : {result_json}");
+
+        result
+    })
+}
+
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "UPPERCASE")]
+pub struct ConfigEnv {
+    pub redis_url: String,
+}
